bccd6402fb712ee7ff1465bdc3282c3c53fa8136
sanguk_han <sanguk_han@tmax.co.kr>
2022-08-01 17:48:31 +0900

    room chat

--- server.js
+++ server.js
@@ -6,10 +6,43 @@ const io = new Server(3000, {
     }
     /* options */ });
 
-
+let room = ['room1', 'room2'];
+let a= 0;
 console.log("server start");
 
+io.on('connection',(socket)=>{
 
+    socket.on('disconnect',()=>{
+        console.log('user disconnected');
+    });
+
+    socket.on('leaveRoom',(num,name)=>{
+        socket.leave(room[num],()=>{
+            console.log(name + ' leave a ' + room[num]);
+            io.to(room[num]).emit('leaveRoom', num, name);
+        });
+    });
+
+    socket.on('joinRoom', (num, name)=>{
+        socket.join(room[num], ()=>{
+
+            console.log(name + ' join a '+ room[num]);
+            io.to(room[num]).emit('joinRoom',num,name);
+        });
+    });
+
+    socket.on('chat message', (num, name, msg)=>{
+        a = num;
+        io.to(room[a]).emit('chat message',name,msg);
+        console.log(num+ ' message arrived : ' + msg);
+    });
+
+
+});
+
+
+
+/*
 const namespace1 = io.of('/namespace1');
 namespace1.on('connection',(socket)=>{
 
@@ -32,7 +65,10 @@ namespace2.on('connection',(socket)=>{
         console.log(reason);
         console.log("namespace2 disconnect");
     })
-})
+})*/
+
+
+
 /*
 
 io.on("connection", (socket) => {
